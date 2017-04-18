12d04c507f8c313af68a66cc63a2a6e1eab02952
Max Horowitz-Gelb <Max Horowitz-Gelb>
2017-04-18 16:00:42 -0500

    added mising code for kde

--- cs761/hw3/KdeNW/KdeNW/KdeNW/Program.cs
+++ cs761/hw3/KdeNW/KdeNW/KdeNW/Program.cs
@@ -11,5 +11,20 @@ namespace KdeNW
         static void Main(string[] args)
         {
         }
+
+        
+    }
+
+    public class KdeNW
+    {
+        private double[] _x;
+        private double[] _y;
+        private 
+        public KdeNW(double[] x, double[] y, double bandwidth)
+        {
+            _x = x;
+            _y = y;
+
+        }
     }
 }
